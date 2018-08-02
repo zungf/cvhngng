cf0de95b0fe1ea5e6260055ee90ec1d50ffcc456
zungf <41270843+zungf@users.noreply.github.com>
2018-08-02 21:30:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -298,6 +298,9 @@ message.channel.sendMessage('تـم إنـشاء روم كـتابـي')
 
 }
 });
+
+
+
 client.on("message", (message) => {
 if (message.content.startsWith("=cv")) {
             if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
@@ -731,6 +734,33 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+var prefix = "=";
+
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    if (!args[1]) {
+message.channel.send("**bc <message>**");
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
 
 
 
